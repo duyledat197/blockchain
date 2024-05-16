4eb2520ed54722fe362db9f4b9502d996b157255
myth <myth@trin-tech.net>
2024-05-16 22:13:23 +0700

    [DOC] fix doc

--- internal/user-mgnt/repositories/user.go
+++ internal/user-mgnt/repositories/user.go
@@ -6,8 +6,13 @@ import (
 	"openmyth/blockchain/internal/user-mgnt/entities"
 )
 
+// UserRepository defines the interface for the user repository
+// It provides methods to create and find users
 type UserRepository interface {
+	// Create creates a new user
 	Create(ctx context.Context, data *entities.User) error
+	// FindUser finds a user by its ID
 	FindUser(ctx context.Context, id string) (*entities.User, error)
+	// FindUserByUsername finds a user by its username
 	FindUserByUsername(ctx context.Context, username string) (*entities.User, error)
 }
