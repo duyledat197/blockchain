acdcffd2df1e795c4639828f4d60de3fd5b8c919
myth <myth@trin-tech.net>
2024-05-17 16:03:55 +0700

    [REFACTOR] refactor frontend cmd

--- cmd/srv/frontend/srv.go
+++ cmd/srv/frontend/srv.go
@@ -2,9 +2,11 @@ package frontend
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net/http"
 
+	"openmyth/blockchain/config"
 	"openmyth/blockchain/internal/contract/services"
 	"openmyth/blockchain/pkg/iface/processor"
 )
@@ -24,12 +26,11 @@ func NewServer() *Server {
 
 // loadServer initializes the server to listen on a specified port for HTTP requests.
 func (s *Server) loadServer() {
-	port := s.service.Cfg.Frontend.Port
-	handler := http.FileServer(http.Dir("/html"))
-	log.Printf("server listening in port: %v", port)
-	if err := http.ListenAndServe(s.service.Cfg.Frontend.Address(), handler); err != nil {
-		log.Fatalf("failed to serve: %v", err)
+	fileSrv := &fileServer{
+		endpoint: s.service.Cfg.Frontend,
 	}
+
+	s.service.WithProcessors(fileSrv)
 }
 
 // Run runs the server with the provided context.
@@ -38,4 +39,33 @@ func (s *Server) Run(ctx context.Context) {
 	s.service.LoadConfig()
 
 	s.loadServer()
+
+	s.service.GracefulShutdown(ctx)
+}
+
+type fileServer struct {
+	server *http.Server
+
+	endpoint *config.Endpoint
+}
+
+// Start starts the file server to listen on a specified port.
+func (s *fileServer) Start(_ context.Context) error {
+	port := s.endpoint.Port
+	handler := http.FileServer(http.Dir("/html"))
+	log.Printf("server listening in port: %v", port)
+	s.server = &http.Server{
+		Addr:    fmt.Sprintf(":%s", port),
+		Handler: handler,
+	}
+	if err := s.server.ListenAndServe(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Stop stops the file server.
+func (s *fileServer) Stop(_ context.Context) error {
+	return s.server.Close()
 }
